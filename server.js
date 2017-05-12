c1583cbefbd132909a49d004a033fafd464f1ea2
Isaac <isaacxpreston@gmail.com>
2017-05-12 15:18:10 -0400

    update:

--- server.js
+++ server.js
@@ -5,12 +5,18 @@ var port = process.env.PORT || 3000
 
 var token;
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
-app.get('/', function (req, res) {
+
+app.get('*', function (req, res) {
 
   //construct the request
   var authRequest = {"Database": "Tufenkian2007","UID": "ISAAC","Password": "verbalplusvisual2"}
-  
+
   var auth_url = "http://74.95.35.226:6060/MaximizerWebData/Data.svc/json/Authenticate"
 
   axios.post(auth_url, JSON.stringify(authRequest))
