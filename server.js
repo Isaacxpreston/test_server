9f3e7ff6855dd35ee3145b937f3a57d34524c4ff
Isaac <isaacxpreston@gmail.com>
2017-05-15 09:34:51 -0400

    update

--- server.js
+++ server.js
@@ -12,8 +12,7 @@ app.use(function(req, res, next) {
   next();
 });
 
-
-var addNewEntry = (token, context) => {
+var addNewEntry = (token, context, request) => {
   var post_url = "http://74.95.35.226:6060/MaximizerWebData/Data.svc/json/AbEntryCreate"
   var createRequest = {
     Token: token,
@@ -21,58 +20,66 @@ var addNewEntry = (token, context) => {
       "Data": {
         "Key": null,
         "Type": "Individual",
-        "LastName": "Preston",
-        "FirstName": "Isaac",
-        "Email": "isaac@verbalplusvisual.com",
-        "Phone": "267-243-6875",
+        "LastName": request.LastName,
+        "FirstName": request.FirstName,
+        "Email": request.Email,
+        "Phone": request.Phone,
         "Udf/$NAME(Leads\\GetNewsletter)": [
           "2"
-        ],
-        "Udf/$NAME(Sales\\Lead status)": [
-          "57998"
         ]
+        // "Udf/$NAME(Sales\\Lead status)": [
+        //   "57998"
+        // ]
       }
     }
-  };
+  }
 
   axios.post(post_url, JSON.stringify(createRequest))
   .then(function (response, err) {
-    console.log(response.data)
     context.send("success!")
   })
   .catch((err) => {
     context.send("server error")
   })
 }
+
+
 app.get('*', function (req, res) {
   res.send('Server running on port ' + port)
 })
 
 app.post('*', function (req, res) {
+  var authRequest = {"Database": "Tufenkian2007","UID": "ISAAC","Password": "verbalplusvisual2"}
+  var requestObject = {
+    "FirstName": req.body.FirstName,
+    "LastName": req.body.LastName,
+    "Email": req.body.Email,
+    "Phone": req.body.Phone
+  }
+  var auth_url = "http://74.95.35.226:6060/MaximizerWebData/Data.svc/json/Authenticate"
 
-  console.log("requested with", req.body)
-  res.send("you sent", req.body)
-
-  // var authRequest = {"Database": "Tufenkian2007","UID": "ISAAC","Password": "verbalplusvisual2"}
-
-  // var auth_url = "http://74.95.35.226:6060/MaximizerWebData/Data.svc/json/Authenticate"
-
-  // axios.post(auth_url, JSON.stringify(authRequest))
-  // .then(function (response, err) {
-  //   if(response.data["Code"] === 0) {
-  //     var token = response.data["Data"]["Token"]
-  //     addNewEntry(token, res)
-  //   } else {
-  //     res.send("invalid credentials")
-  //   }
-  // })
-  // .catch((err) => {
-  //   console.log("ERROR", err.data)
-  //   res.send("server error")
-  // })
-  
+  axios.post(auth_url, JSON.stringify(authRequest))
+  .then(function (response, err) {
+    if(response.data["Code"] === 0) {
+      var token = response.data["Data"]["Token"]
+      addNewEntry(token, res, requestObject)
+    } else {
+      res.send("invalid credentials")
+    }
+  })
+  .catch((err) => {
+    console.log("ERROR", err.data)
+    res.send("server error")
+  })
 })
 
 app.listen(port, function () {
   console.log("running on " + port)
 })
+
+// {
+// 	"FirstName": "Isaac",
+// 	"LastName": "Preston",
+// 	"Email": "isaac@verbalplusvisual.com",
+// 	"Phone": "267-243-6875"
+// }
